0527a74a768cc773e62ad1b88c4c03ea5cddcae8
Anthony Lane Jr <TonyLane2017@gmail.com>
2022-08-27 12:33:17 -0700

    user module v.1

--- users/models.py
+++ users/models.py
@@ -1,4 +1,58 @@
 from pydoc import describe
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+
+class Frequency(models.Model):
+    name = models.CharField(max_length=128)
+    description =models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Gender(models.Model):
+    name = models.CharField(max_length=128)
+    description =models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class AgeGroup(models.Model):
+    name = models.CharField(max_length=128)
+    description =models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age_category = models.ForeignKey(
+        AgeGroup,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    weight = models.CharField(max_length=30)
+    height = models.CharField(max_length=30)
+    gender = models.ForeignKey(
+        Gender,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    exercise_frequency = models.ForeignKey(
+        Frequency,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    description =models.TextField()
+
+
+class UserList(CustomUser):
+    super().__init__()
+    name = 'User List'
+    users = []
+
+    def add_user(self, user):
+        self.users.append(user)
