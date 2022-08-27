738cc26bdd24e836e3e695866e2080437c01ba92
Anthony Lane Jr <TonyLane2017@gmail.com>
2022-08-27 11:21:15 -0700

    workout models

--- workout/models.py
+++ workout/models.py
@@ -1,3 +1,62 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your models here.
+
+class Workout(models.Model):
+    def __init__(self):
+        super().__init__()
+    name = models.CharField(max_length=128)
+    exercise_1 = models.TextField()
+    description_1 = models.TextField()
+    exercise_2 = models.TextField()
+    description_2 = models.TextField()
+    exercise_3 = models.TextField()
+    description_3 = models.TextField()
+    exercise_4 = models.TextField()
+    description_4 = models.TextField()
+    exercise_5 = models.TextField()
+    description_5 = models.TextField()
+    goal_focus = goal()
+
+
+    def goal(self, input):
+        if input.lower() == 'weight loss':
+            return input
+        if input.lower() == 'weight gain':
+            return input
+        else:
+            return 'maintain weight'
+
+class Workout_template(Workout):
+    def __init__(self):
+        super().__init__()
+    goal = goal_choice()
+    name = goal
+    week_1 = []
+    week_2 = []
+    week_3 = []
+    week_4 = []
+
+
+    def goal_choice(self, answer):
+        if answer == 'weight loss':
+            return answer
+        elif answer == 'weight gain':
+            return answer
+        else:
+            return 'maintain weight'
+
+
+    def append_workout(self, workout, week):
+        if week == 1:
+            self.week_1.append(workout)
+        elif week == 2:
+            self.week_2.append(workout)
+        elif week == 3:
+            self.week_3.append(workout)
+        elif week == 4:
+            self.week_4.append(workout)
+        else:
+            return 'Please enter a valid week.'
+    
--- workout_templates/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WorkoutTemplatesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workout_templates'
