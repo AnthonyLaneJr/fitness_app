fe23312aafdfbb5dc42d34ddb8ca154d860ba443
Anthony Lane Jr <TonyLane2017@gmail.com>
2022-09-07 17:27:11 -0700

    sqlite switch

--- config/settings.py
+++ config/settings.py
@@ -81,13 +81,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': '529736',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
