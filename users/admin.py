e6d0e02f6dbe898651704b2ef2a328e9e7ebc225
Anthony Lane Jr <TonyLane2017@gmail.com>
2022-09-07 18:12:50 -0700

    db fixed

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import AgeGroup, FitnessUser, UserList, Frequency, Gender
 # Register your models here.
+
+admin.site.register(AgeGroup)
+admin.site.register(FitnessUser)
+admin.site.register(UserList)
+admin.site.register(Frequency)
+admin.site.register(Gender)
