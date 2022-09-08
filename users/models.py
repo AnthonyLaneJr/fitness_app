2350be6d6b0620800b8e8896672414b521a5d9e0
Anthony Lane Jr <TonyLane2017@gmail.com>
2022-09-07 18:05:39 -0700

    db fix

--- users/models.py
+++ users/models.py
@@ -24,6 +24,7 @@ class AgeGroup(models.Model):
         return self.name
 
 class FitnessUser(AbstractUser):
+    password = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age_category = models.ForeignKey(
@@ -46,7 +47,8 @@ class FitnessUser(AbstractUser):
         blank=True,
         null=True
     )
-    description =models.TextField()
+    description = models.TextField()
+    USERNAME_FIELD: str
 
 
 class UserList(models.Model):
