053a4670641d7e822d3ff75c3babcb46a0a6909b
Anthony Lane Jr <TonyLane2017@gmail.com>
2022-10-03 17:45:14 -0700

    backend final touches

--- users/admin.py
+++ users/admin.py
@@ -3,12 +3,6 @@ from .models import AgeGroup, FitnessUser, Gender
 from workout.models import Workout_template
 # Register your models here.
 
-
-# ------  not functioning ------ #
-class FitnessUserAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"template_id": '(Workout_template.objects.get(frequency = "exercise_frequency", goal = "goal" ).pk)'}
-
-
 admin.site.register(AgeGroup)
 admin.site.register(FitnessUser)
 admin.site.register(Gender)
